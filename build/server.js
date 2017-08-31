2f86c8453a824292335c543eb5e108a4397cc810
Mathieu Rousseau <mathieu0709@gmail.com>
2017-08-31 12:01:43 +0200

    Update server.js

--- build/server.js
+++ build/server.js
@@ -60,5 +60,5 @@ app.get('*', production ? doSSR : (req, res) => {
 });
 
 http.createServer(app).listen(PORT, _ => {
-  console.log('listening on http://localhost:8080');
+  console.log(`listening on http://localhost:PORT`);
 });
